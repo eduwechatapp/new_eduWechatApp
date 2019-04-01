b90b6c6b23a010a281f02cd3c76f25e70d1000a0
R <787602141@qq.com>
2019-04-01 18:47:33 +0800

    add some

---
+++ pages/inner_list/inner_list.js
@@ -0,0 +1,82 @@
+const request = require('../../utils/request');
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    menuList:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this,
+    path = options.path,
+    args = options.args
+
+    
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  // 获取列表信息
+  getMenuList:function(path,args) {
+    var that = this
+    
+    request.post(path,args).then(res=>{
+      
+      console.log(res)
+    })
+  }
+})
