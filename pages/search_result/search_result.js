7dabd2bdf335ffae7160d32783b856e386543058
obdormio <17738729134@163.com>
2019-05-20 00:53:55 +0800

    添加查看单科所有搜索结果的入口

--- pages/search_result/search_result.js
+++ pages/search_result/search_result.js
@@ -1,16 +1,22 @@
-var globalData = getApp()
+import { post } from '../../utils/request';
+
+const globalData = getApp();
+
+const Data = {
+  subjectEnum: ['yy', 'sx', 'yw', 'hx', 'wl', 'sw', 'dl', 'zz', 'ls'],
+};
+
 Page({
-  data:{
-    subject:'',
-    model:'',
-    search:'',
-    contentList:[],
-    scrollTop:0,
-    dataset_id:'',
-    id:'',
-    module:'搜索',
-    show:true,
-    subjectLUT:[
+  data: {
+    subject: '',
+    model: '',
+    search: '',
+    contentList: [],
+    scrollTop: 0,
+    dataset_id: '',
+    id: '',
+    module: '搜索',
+    subjectLUT: [
       { sub: "数学", index: 0 },
       { sub: "语文", index: 1 },
       { sub: "英语", index: 2 },
@@ -19,127 +25,120 @@ Page({
       { sub: "生物", index: 5 },
       { sub: "地理", index: 6 },
       { sub: "历史", index: 7 },
-      { sub: "政治", index: 8 }
+      { sub: "政治", index: 8 },
     ],
-    index:''
+    index: '',
   },
-  onLoad:function(options){
-    var that = this;
-    var str = options.array;
-    var array = str.split(',');
-    var noResult = true;
-    console.log(array)
-    if(array[1]=="cancel" && array[2]=="cancel"){//简单搜索
-      wx.showToast({
-        title: '加载ing',
-        icon: 'loading'
-      })
-      wx.request({
-        url: 'https://www.vaskka.com/mp/search/simple/'+'test/'+array[0]+'/5/0',
-        method:'POST',
-        header: {
-          "Accept": "application/json"
-        },
-        success: function (res) {
-          wx.hideToast();
-          console.log(res.data.data)
-          
-          that.setData({
-            contentList: res.data.data,
-            input: array[0]
-          })
-          for(var i =0;i<9;i++){
-            if(that.data.contentList[i].dataList!=0){
-              noResult = false
-            }
-          }
-          if (noResult) {
-            wx.showModal({
-              title: '提示',
-              content: '查询无结果，建议更换关键字再次搜索哦',
-              showCancel: false,
-              success(res) {
-                if (res.confirm) {
-                  wx.navigateBack({
-                    delta: 1
-                  })
-                }
-              }
-            })
-          }
-          globalData.simpleList = res.data.data
-         
-          //console.log(res.data.data.content);
-          //console.log("content:"+that.data.menuList[0].content);
-        }
-      })
-    }
-    else{//复杂搜索
-      var detail
-      if(array[2]=="title"||array[2]=="cancel"){
-        detail={
-          "title" : array[0]
-          }
-      }
-      if (array[2] == "content") {
-        detail = {
-          "content": array[0]
-        }
-      }
-      console.log(detail)
-      wx.showToast({
-        title: '加载ing',
-        icon: 'loading'
-      })
-      wx.request({
-        url: 'https://www.vaskka.com/mp/search/detail/' + 'test/' + array[1] + '/100/0',
-        method: 'POST',
-        data:detail,
-        header: {
-          "Accept": "application/json"
-        },
-        success: function (res) {
-          
-          console.log(res.data.data)
-          that.setData({
-            contentList: res.data.data,
-            input: array[0],
-            show:false
-          })
-          globalData.contentList = that.data.contentList
-          var contentListString = JSON.stringify(that.data.contentList.dataList)
-          wx.hideToast()
-        wx.navigateTo({
-          url: '../inner_list/inner_list?array=' + ['', that.data.contentList.subject, that.data.module]
-        })
-          //console.log(res.data.data.content);
-          //console.log("content:"+that.data.menuList[0].content);
-        }
-      })
+
+  onLoad(options) {
+    const that = this;
+    const { searchValue, subject, searchMode } = options;
+    if (subject === '-1' && searchMode === '-1') { // 简单搜索
+      this.simpleSearch(searchValue);
+    } else {
+      this.advanceSearch(searchValue, subject, searchMode);
     }
   },
+
+  onNoResult() {
+    wx.showModal({
+      title: '提示',
+      content: '查询无结果，建议更换关键字再次搜索哦',
+      showCancel: false,
+      success(res) {
+        if (res.confirm) {
+          wx.navigateBack({
+            delta: 1
+          });
+        }
+      },
+    });
+  },
+
+  /**
+   * 简单搜索
+   */
+  async simpleSearch(key) {
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+    });
+
+    const response = await post(`/search/simple/test/${key}/5/0`);
+    wx.hideToast();
+
+    if (response.data.every(e => e.dataList.length === 0)) {
+      this.onNoResult();
+      return;
+    }
+
+    this.setData({
+      contentList: response.data,
+    });
+  },
+
+  /**
+   * 高级搜索
+   */
+  async advanceSearch(key, subject, mode) {
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+    });
+
+    let data;
+    if (mode === '1') { // 按内容搜索
+      data = { content: key };
+    } else {
+      data = { title: key };
+    }
+
+    const response = await post(`/search/detail/test/${Data.subjectEnum[subject]}/5/0`, {}, data);
+    wx.hideToast();
+
+    if (response.data.dataList.length === 0) {
+      this.onNoResult();
+      return;
+    }
+
+    this.setData({
+      contentList: response.data,
+    });
+
+    globalData.contentList = response.data.contentList;
+    wx.hideToast();
+    wx.navigateTo({
+      url: '../inner_list/inner_list?array=' + ['', response.data.contentList.subject, this.data.module]
+    });
+  },
+
   onPageScroll(e) {
     this.setData({
       scrollTop: e.scrollTop,
-    })
+    });
   },
-  toText:function(e){
+
+  toDetail(e) {
+    console.log(e.currentTarget.dataset.name);
+  },
+
+  toText(e) {
     var that = this;
     console.log(e)
     that.setData({
       dataset_id: e.currentTarget.dataset.id,
-      id:e.currentTarget.id
+      id: e.currentTarget.id
     })
     var i
-    for(i=0;i<9;i++){
-      if(that.data.subjectLUT[i].sub==that.data.dataset_id){
+    for (i = 0; i < 9; i++) {
+      if (that.data.subjectLUT[i].sub == that.data.dataset_id) {
         that.setData({
           index: i
         })
       }
     }
     wx.navigateTo({
-      url: '../content/content?array=' + [that.data.dataset_id, that.data.id, that.data.module] + '&text=' + [that.data.index, that.data.id,that.data.input]
+      url: '../content/content?array=' + [that.data.dataset_id, that.data.id, that.data.module] + '&text=' + [that.data.index, that.data.id, that.data.input]
     })
   }
-})
+});
