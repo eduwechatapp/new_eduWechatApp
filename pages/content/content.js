59c0db4970d56051b9c07c8391889e75ebd148d4
obdormio <17738729134@163.com>
2019-05-20 21:11:08 +0800

    更新文章详情页面的样式

--- pages/content/content.js
+++ pages/content/content.js
@@ -1,12 +1,20 @@
-const WxParse = require('../../wxParse/wxParse.js');
+import WxParse from '../../wxParse/wxParse';
+
 const app = getApp();
 
 Page({
   data: {
+    select: 0,
   },
 
   onLoad() {
     WxParse.wxParse('article', 'html', app.globalData.content, this, 5);
     app.globalData.content = '';
+  },
+
+  changeState(e) {
+    this.setData({
+      select: e.currentTarget.dataset.id,
+    });
   },
 });
