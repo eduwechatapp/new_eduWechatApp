abce1b280753dbe81648fee99cf7c10ab1f7cf9a
obdormio <17738729134@163.com>
2019-05-21 11:11:05 +0800

    触底加载(实验性)

--- pages/inner_list/inner_list.js
+++ pages/inner_list/inner_list.js
@@ -1,26 +1,60 @@
 const app = getApp();
 
+const Data = {
+  currentPage: 0,
+  pageSize: 20,
+  subject: '',
+  searchValue: '',
+  searchMode: '',
+};
+
 Page({
   data: {
     menuList: [],
   },
 
   onLoad(options) {
-    const { subject, searchValue, searchMode } = options;
-    this.fetchData(subject, searchValue, searchMode);
+    Data.subject = options.subject;
+    Data.searchValue = options.searchValue;
+    Data.searchMode = options.searchMode;
+    this.fetchData();
   },
 
-  async fetchData(subject, searchValue, searchMode) {
-    const data = searchMode === '1' ? { content: searchValue } : { title: searchValue };
-    const response = await app.post(`/search/detail/test/${app.globalData.subjectEnum[subject].unique}/10/0`, {}, data);
-    
+  toast(title, icon, duration) {
+    return new Promise(resolve => {
+      wx.showToast({
+        title,
+        icon,
+        duration,
+        success() {
+          resolve();
+        },
+      });
+    });
+  },
+
+  async fetchData() {
+    await this.toast('加载中', 'none', 2000);
+  
+    const data = Data.searchMode === '1' ? { content: Data.searchValue } : { title: Data.searchValue };
+    const url = `/search/detail/test/${app.globalData.subjectEnum[Data.subject].unique}/${Data.pageSize}/${Data.currentPage}`;
+    const response = await app.post(url, {}, data);
+
+    wx.hideToast();
+
     if (response.data.dataList.length === 0) {
-      this.onNoResult();
-      return;
+      if (this.data.menuList.length === 0) {
+        this.onNoResult();
+        return;
+      }
+
+      await this.toast('没有更多数据了', 'none', 2000);
     }
 
+    await this.toast('加载成功', 'success', 2000);
+
     this.setData({
-      menuList: response.data.dataList,
+      menuList: response.data.dataList.concat(this.data.menuList),
     });
   },
 
@@ -37,5 +71,9 @@ Page({
         }
       },
     });
+  },
+
+  refresh() {
+    this.fetchData();
   },
 });
