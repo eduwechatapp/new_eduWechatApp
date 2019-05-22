c1c8111a5346c47f4693cd01341300c8052b03b9
so_fann <1363224536@qq.com>
2019-05-22 22:53:31 +0800

    题目列表页面

--- pages/question_list/question_list.js
+++ pages/question_list/question_list.js
@@ -1,11 +1,14 @@
+import WxParse from '../../wxParse/wxParse';
 Page({
   data:{
     yiji:'',
     erji:'',
-    subject:''
+    subject:'',
+    contentList:''
   },
   onLoad:function(options){
     var that = this;
+    var array = new Array();
     console.log(options)
     that.setData({
       yiji:options.yiji,
@@ -16,6 +19,21 @@ Page({
       url: `http://129.204.216.249:4000/exercise/list/test/${that.data.subject}/${that.data.yiji}/${that.data.erji}/5/1`,
       success(res){
         console.log(res)
+        that.setData({
+          contentList: res.data.data
+        })
+        
+        that.data.contentList.forEach(function(value,index){
+          console.log(value.content)
+          array.push(value.content)
+        })
+        console.log(array)
+        for (let i = 0; i < array.length; i++) {
+          WxParse.wxParse('array' + i, 'html', array[i], that);
+          if (i === array.length - 1) {
+            WxParse.wxParseTemArray("array", 'array', array.length, that)
+          }
+        }
       }
     })
   }
