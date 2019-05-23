c790c0c8d8b53574e619f3ab704a2a74a6681249
so_fann <1363224536@qq.com>
2019-05-23 17:54:05 +0800

    新增题库详情页面

--- pages/question_list/question_list.js
+++ pages/question_list/question_list.js
@@ -20,7 +20,7 @@ Page({
     that.setData({
       yiji:options.yiji,
       erji:options.erji,
-      subject: options.subject
+      subject: options.subject,
     })
     wx.request({
       url: `http://129.204.216.249:4000/exercise/list/test/${that.data.subject}/${that.data.yiji}/${that.data.erji}/5/1`,
