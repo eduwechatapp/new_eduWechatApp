6ff6f62a8f9b09222a958eb16f17802d203a29c4
so_fann <1363224536@qq.com>
2019-05-27 16:01:58 +0800

    题目详情选择性显示

--- pages/question_detail/question_detail.js
+++ pages/question_detail/question_detail.js
@@ -1,25 +1,12 @@
 import WxParse from '../../wxParse/wxParse';
 Page({
   data:{
-    detailInfo:{
-      "yiji": "集合与常用逻辑用语",
-      "erji": "集合与常用逻辑用语",
-      "content": " <p>已知集合<img src=\"https://tiku.21cnjy.com/tikupic/7f/d5/7fad5f1345383cc451bfc9c5512187b6.png\" style=\"vertical-align:middle;\">，则  （ ）</p>", 
-      "answer": "<p>B</p>", 
-      "type": "单选题", 
-      "analysis": {
-        "question_analysis":"<p>由<img src=\"https://tiku.21cnjy.com/tikupic/28/d6/28dd6d3f542756dacc079bcf4e6506ad.png\"     style=\"vertical-align:middle;\"> ，所以集合<img src=\"https://tiku.21cnjy.com/tikupic/1f/c9/1fac903ec4b76119b786fd75fc9da3a3.png\" style=\"vertical-align:middle;\"> ，所以A∩B≠<img src=\"https://tiku.21cnjy.com/tikupic/36/be/36abe9afe2bb5c0a8c760b7af837d537.png\" style=\"vertical-align:middle;\"> <br>A∪B=R，故B正确，故选B",
-      "question_point":"本题考查集合的交集，并集的运算，集合与集合的关系<br>",
-      "question_comment":"<p>解决本题的关键是解一元二次不等式，求出集合A</p>",
-      },
-      "choose":
-        ["<p>A．A∩B=Æ</p>",
-        "<p>B．A∪B=R</p>",
-        "<p>C．B⊆A </p>",
-        "<p>D．A⊆B</p>"],
-      "answer_index": [1]
-    },
-    id:''
+    detailInfo:{},
+    id:'',
+    haveExamPoint: false,
+    haveAnalysis: false,
+    haveAnswer: false,
+    haveComment: false
   },
   async onLoad(options) {
     await this.Set({
@@ -39,10 +26,31 @@ Page({
         WxParse.wxParseTemArray("choose", 'choose', length, that)
       }
     }
-    WxParse.wxParse('exampoint', 'html', that.data.detailInfo.analysis.question_point, that)
-    WxParse.wxParse('analysis', 'html', that.data.detailInfo.analysis.question_analysis, that)
-    WxParse.wxParse('answer', 'html', that.data.detailInfo.answer, that)
-    WxParse.wxParse('comment', 'html', that.data.detailInfo.analysis.question_comment, that)
+    if (that.data.detailInfo.analysis.question_point != null){
+      await this.Set({
+        haveExamPoint: true,
+      });
+      WxParse.wxParse('exampoint', 'html', that.data.detailInfo.analysis.question_point, that)
+    }
+    if (that.data.detailInfo.analysis.question_analysis != null) {
+      await this.Set({
+        haveAnalysis: true,
+      });
+      WxParse.wxParse('analysis', 'html', that.data.detailInfo.analysis.question_analysis, that)
+    }
+    if (that.data.detailInfo.answer != null) {
+      await this.Set({
+        haveAnswer: true,
+      });
+      WxParse.wxParse('answer', 'html', that.data.detailInfo.answer, that)
+    }
+    if (that.data.detailInfo.analysis.question_comment != null) {
+      await this.Set({
+        haveComment: true,
+      });
+      WxParse.wxParse('comment', 'html', that.data.detailInfo.analysis.question_comment, that)
+    }
+    
   },
   async fetchData() {
     return new Promise(resolve => {
