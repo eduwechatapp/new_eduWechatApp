c561e0a3d7ab3c2f59280cf74da20ccc85e0c692
obdormio <17738729134@163.com>
2019-06-01 17:24:15 +0800

    移除 app 中无用的函数

--- app.js
+++ app.js
@@ -80,39 +80,6 @@ App({
     })
   },
 
-  post(_url, urlParam = {}, data = {}) {
-    let url = `${host}${_url}`;
-    if (Object.keys(param).length > 0) {
-      urlParam = '?';
-      Object.keys(param).forEach((k, i) => {
-        if (i > 0) {
-          urlParam += '&';
-        }
-        urlParam += `${k}=${param[k]}`;
-      });
-    }
-    return new Promise((resolve, reject) => {
-      wx.request({
-        url,
-        data,
-        method: 'POST',
-        success(res) {
-          resolve(res.data);
-        },
-        fail(res) {
-          reject(res.data)
-          wx.showToast({
-            title: '网络错误!', //提示的内容,
-            icon: 'none', //图标,
-            duration: 2000, //延迟时间,
-            mask: true, //显示透明蒙层，防止触摸穿透,
-            success: res => { }
-          });
-        }
-      });
-    });
-  },
-
   route(_url, param = {}) {
     let urlParam = '';
     if (Object.keys(param).length > 0) {
@@ -143,16 +110,6 @@ App({
     return new Promise(res => {
       wx.hideToast({
         success: res,
-      });
-    });
-  },
-
-  scrollTo(pos, duration = 300) {
-    return new Promise(res => {
-      wx.pageScrollTo({
-        scrollTo: pos,
-        duration,
-        complete: res,
       });
     });
   },
