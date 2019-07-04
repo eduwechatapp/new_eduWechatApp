af488d49ca3994342d4b85b4530fdcdd0186fde0
obdormio <17738729134@163.com>
2019-07-04 20:37:31 +0800

    添加视频

--- pages/video/video.js
+++ pages/video/video.js
@@ -1,3 +1,25 @@
-Page({
+const app = getApp();
 
-})
+Page({
+  data: {
+    videoList: [],
+  },
+
+  async onShow() {
+    await this.fetchData();
+  },
+
+  async fetchData() {
+    const videoList = [{
+      title: '疯狂动物城',
+      src: 'https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
+    }];
+    this.setData({ videoList });
+  },
+
+  toDetail(e) {
+    const index = e.currentTarget.dataset.index;
+    const { title, src } = this.data.videoList[index];
+    app.route('/pages/video/detail/detail', { title, src });
+  },
+});
